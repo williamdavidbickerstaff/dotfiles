d664e3ae93776f447b8dcd9905f817ee13b13465
will <will@Williams-iMac.arts.local>
2024-02-15 12:31:08 +0000

    adding vim-tmux functionality

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,3 +8,12 @@ vim.keymap.set(
   require("telescope.builtin").resume,
   { noremap = true, silent = true, desc = "Resume" }
 )
+
+vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("i", "kj", "<ESC>")
+
+-- vim-tmux-navigator
+vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>", { silent = true })
+vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>", { silent = true })
+vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>", { silent = true })
+vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>", { silent = true })
---
+++ nvim/lua/plugins/plugins.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "christoomey/vim-tmux-navigator",
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    keys = {
+      {
+        "<leader>sB",
+        ":Telescope file_browser path=%:p:h=%:p:h<cr>",
+        desc = "Browse Files",
+      },
+    },
+    config = function()
+      require("telescope").load_extension("file_browser")
+    end,
+  },
+}
